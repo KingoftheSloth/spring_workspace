3fa5bbc26354dc158fc64c37135a73c049a09a10
KingoftheSloth <131847767+KingoftheSloth@users.noreply.github.com>
2023-09-26 17:47:39 +0900

    회원가입 만들기 성공

--- spring_3board/src/main/java/org/iclass/mvc/controller/MemberController.java
+++ spring_3board/src/main/java/org/iclass/mvc/controller/MemberController.java
@@ -1,9 +1,15 @@
 package org.iclass.mvc.controller;
 
 import org.iclass.mvc.dto.BookUser;
+import org.iclass.mvc.service.BookUserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -13,10 +19,34 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/member")
 public class MemberController { // 회원가입을 구현해보기 
 
+	private final BookUserService service;
+	private MemberController(BookUserService service) {
+		this.service = service;
+	}
+	
 	@RequestMapping("/test")
 	public void test(@SessionAttribute("user") BookUser user) { // 로그인 방법 1로 되돌려 놓고 테스트 합니다.
 		//LoginController 에서 저장된 user 세션 애트리뷰트를 가져 옵니다.
 		log.info("user test : {}" , user);		
 	}
 	
-}
+	@GetMapping("/join")
+	public void join() {
+		
+	}
+	
+	@PostMapping("/join")
+	public String registerAction(BookUser dto,
+			RedirectAttributes redirectAttributes){
+		log.info("dto : {}" , dto);
+		service.join(dto);
+		redirectAttributes.addFlashAttribute("message", "가입 성공 이제 꺼져");
+		return "redirect:/member/welcome";
+		
+	}
+	
+	@GetMapping("/welcome")
+		public void welcome() {
+			
+		}
+	}
---
+++ spring_3board/src/main/webapp/WEB-INF/views/member/welcome.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>welcome</title>
+</head>
+<body>
+<h3>이제 꺼져</h3>
+<hr>
+
+</body>
+<script type="text/javascript">
+if('${message}'.length !=0)	alert('${message}');</script>
+</html>
